632a40083fa256ecb60bdd4ba88271580f08d05f
Nikhil Sreedhar(UST <150619@ust-global.com>
2021-10-26 19:26:43 +0530

    Redux Store created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './store';
+
 //import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+  <Provider store={store} >
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ frontend/src/store.js
@@ -0,0 +1,18 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import data from './data';
+
+
+// redux Store
+
+
+const initialState = {};
+const reducer = (state, action) =>{
+    return {products: data.products}
+}
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer,initialState, composeEnhancer(applyMiddleware(thunk)));
+
+export default store;
